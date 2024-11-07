b6b8b26f3f5a43eab6d16b85f5e9a72e8e4048b8
thisiscleverson <cleverson2020333@gmail.com>
2024-11-07 15:30:56 -0300

    Configuração para o DbContext StockExchangeContext utilizar o MySQL

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using System.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
 using simulador_de_bolsa_valores_API.Models;
 
 namespace simulador_de_bolsa_valores_API;
@@ -8,8 +11,13 @@ public class Program{
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllers();
-        builder.Services.AddDbContext<ClientContext>(opt => opt.UseInMemoryDatabase("Client"));
         
+        var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+        builder.Services.AddDbContext<StockExchangeContext>(options =>{
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        });
+
         var app = builder.Build();
 
         app.UseHttpsRedirection();
