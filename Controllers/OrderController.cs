21979ba8c9bb69e8a7c212e6457daff6ceb29f51
thisiscleverson <cleverson2020333@gmail.com>
2024-11-08 15:09:32 -0300

    adição dos metodos e respostas http

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,36 +1,46 @@
-using System.Data;
 using Microsoft.AspNetCore.Mvc;
-using simulador_de_bolsa_valores_API.Models;
 
 namespace simulador_de_bolsa_valores_API.Controllers;
 
 [ApiController]
 [Route("api/v1")]
 public class OrderController : ControllerBase{
-   
-   [HttpGet("getOrder")]
-    public async Task<ActionResult<Ordens>> GetOrder([FromQuery] string id){
-    
+
+    private readonly IClientService _clientService;
+
+    public OrderController (IClientService clientService){
+        _clientService = clientService;
     }
+
+
+    [HttpGet("getOrder")]
+    public async Task<ActionResult> GetOrder([FromQuery] string id){
+
+        try{
+            var order = await _clientService.GetOrderById(id);
+            return Ok(order);
+
+        }catch (KeyNotFoundException e){
+            return NotFound(new {message = "Order not found."});
+
+        }
+    }
+
 
     [HttpGet("getOrders")]
-    public async Task<ActionResult<Client>> GetOrders([FromQuery] string account){
-    
+    public async Task<ActionResult> GetOrders([FromQuery] string account){
+        
+        if (!await _clientService.CheckIsClientExists(account)){
+            return NotFound(new {message = "account not found"});
+        }
+
+        var ordens_list = await _clientService.GetAllOrdersByAccount(account);
+        return Ok(ordens_list);
     }
+
 
     [HttpPost("sendNewOrder")]
-    public async Task<ActionResult<Ordens>> PostOrders(){
-    
+    public IActionResult PostOrders(){
+        return Ok();
     }
 }
-
-
-
-
-
-
-
-
-
-        
-
