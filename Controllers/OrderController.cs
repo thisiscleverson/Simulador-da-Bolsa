340439f3c629f03501d95cecb6d41f45bf62fc52
thisiscleverson <cleverson2020333@gmail.com>
2024-11-19 11:22:09 -0300

    formatação de json na resposta da api

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -20,8 +20,18 @@ public class OrderController : ControllerBase{
     public IActionResult GetOrder([FromQuery] string id){
         try{
             Order order = _orderRepository.GetOrderById(id);
-            return Ok(order);
-        
+            
+            var jsonResponse = new {
+                order_id = order.Order_id,
+                account  = order.Account,
+                symbol   = order.Symbol,
+                side     = order.Side ? "BUY":"SELL",
+                price    = order.Price,
+                quantity = order.Qty
+            };
+
+            return StatusCode(200, jsonResponse);
+
         }catch(KeyNotFoundException err){
             return NotFound(new {message = err.Message});
         
@@ -35,8 +45,24 @@ public class OrderController : ControllerBase{
     [HttpGet("getOrders")]
     public async Task<ActionResult> GetOrders([FromQuery] string account){
         try{
-            List<Order> order_list = await _orderRepository.GetAllOrdersByAccount(account);
-            return Ok(order_list);
+            List<Order> orderList = await _orderRepository.GetAllOrdersByAccount(account);
+
+            List<Object> orderFormatted = [];
+
+            foreach(Order order in orderList){
+                    var order_body = new {
+                        order_id = order.Order_id,
+                        account  = order.Account,
+                        symbol   = order.Symbol,
+                        side     = order.Side ? "BUY":"SELL",
+                        price    = order.Price,
+                        quantity = order.Qty
+                    };
+
+                orderFormatted.Add(order_body);
+            }
+
+            return StatusCode(200, orderFormatted);
 
         }catch(KeyNotFoundException err){
             return NotFound(new {message = err.Message});
@@ -63,7 +89,21 @@ public class OrderController : ControllerBase{
             _orderRepository.InsertNewOrder(account, symbol, boolean_side, qty, price);
             _orderRepository.Save();
 
-            return Ok();
+            Order lastAddedItemOrder = _orderRepository.GetLastAddedItemOrderByAccount(account);
+
+            var jsonResponse = new {
+                order_id = lastAddedItemOrder.Order_id,
+                account  = lastAddedItemOrder.Account,
+                symbol   = lastAddedItemOrder.Symbol,
+                side     = lastAddedItemOrder.Side ? "BUY":"SELL",
+                price    = lastAddedItemOrder.Price,
+                quantity = lastAddedItemOrder.Qty
+            };
+
+            return StatusCode(200, new { new_order_response = jsonResponse });
+        }catch(KeyNotFoundException err){
+            return NotFound(new {message = err.Message});
+        
         }catch(Exception err){
             return StatusCode(500, new { message = "Internal server error", error = err.Message});
         }
