61bb4d0cfbcb9eaba99f18e1fd41bbd1021a1575
thisiscleverson <cleverson2020333@gmail.com>
2024-11-27 12:38:56 -0300

    adicao do atributo 'private set' ao model

--- Models/Client.cs
+++ Models/Client.cs
@@ -6,10 +6,10 @@ namespace simulador_de_bolsa_valores_API.Models;
 public class Client{
     [Key]
     [MaxLength(10)]
-    public string Account {get; set; } 
+    public string Account { get; private set; } 
     [NotNull]
     [MaxLength(255)]
-    public string Name {get; set; }
+    public string Name { get; private set;}
 
     public ICollection<Order> Orders { get; set; }
 
--- Models/Order.cs
+++ Models/Order.cs
@@ -11,19 +11,19 @@ public class Order{
     [Key]
     [MaxLength(36)]
     [NotNull]
-    public string Order_id {get; set; }
+    public string Order_id { get; private set; }
     [ForeignKey("Client")]
-    public string Account {get; set;}
-    public Client Client {get; set;}
+    public string Account { get; private set; } 
+    public Client Client { get; private set; } 
     [MaxLength(10)]
-    public string Symbol {get; set;}
+    public string Symbol { get; private set; }
     [DefaultValue(true)]
-    public bool Side {get; set;}
-    public int Qty {get; set;}
+    public bool Side { get; private set; }
+    public int Qty { get; private set; }
     [Precision(6,2)]
-    public decimal Price {get; set;}
+    public decimal Price { get; private set; }
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public DateTime Create_order_at {get; set;}
+    public DateTime Create_order_at { get; private set; }
 
 
     public Order(string account, string symbol, bool side, int qty, decimal price){
